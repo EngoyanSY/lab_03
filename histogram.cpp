4b121ad67018ea68d6accfc6e4a8709de70cd445
EngoyanSY <doggamer13@mail.ru>
2022-04-17 18:22:44 +0300

    build: Модуль histogram

---
+++ histogram.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void
+find_minmax(const vector<double> numbers, double& min, double& max)
+{
+	min = numbers[0];
+	max = numbers[0];
+
+	for (double number : numbers)
+	{
+		if (number < min)
+		{
+			min = number;
+		}
+
+		else
+			if (number > max)
+			{
+				max = number;
+			}
+	}
+}
