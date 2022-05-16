fff89f40b0f13320344a6679aafbd703b3d3e878
EngoyanSY <doggamer13@mail.ru>
2022-05-16 16:25:17 +0300

    code: выбор потока ввода input_numbers()

--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@
 using namespace std;
 
 vector<double> 
-input_numbers(size_t count)
+input_numbers(istream& in, size_t count)
 {
 	vector<double> result(count);
 	for (size_t i = 0; i < count; i++)
 	{
-		cin >> result[i];
+		in >> result[i];
 	}
 	return result;
 }
@@ -65,7 +65,7 @@ int main()
 	}
 
 	cerr << "Enter number: ";
-	const auto numbers = input_numbers(number_count);
+	const auto numbers = input_numbers(cin, number_count);
 
 	size_t bin_count;
 	cerr << "Enter bin count: ";
